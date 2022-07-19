0c0b69f0edf2cd0329b2690368ef9a49b977da15
caroll <maran230216@hotmail.com>
2022-07-19 16:09:26 -0500

    updatetarea1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt = {
+    initialSlide: 0,//slide inicial
+    slidesPerView:1,//slide por vista
+    centeredSlides:true,//los slides esten centrados
+    speed:400//velocidad de transicion de cada slide en milisegundo
+  }
+
   constructor() {}
 
 }
